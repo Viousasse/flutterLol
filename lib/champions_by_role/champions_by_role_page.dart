fda268266aa64d3c359c4c9c32464145b79f48e7
louis.viousase <louis.viousasse@gmail.com>
2026-09-11 15:22:30 +0200

    rajout barre de recherge roles

--- lib/champions_by_role/champions_by_role_page.dart
+++ lib/champions_by_role/champions_by_role_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import '../champions/models/champion.dart';
 import '../champions/services/champion_service.dart';
 import '../champions/widgets/champion_card/champion_card.dart';
+import '../champions/widgets/champions_search_bar/champions_search_bar.dart';
 import '../theme/app_theme.dart';
 
 class ChampionsByRolePage extends StatefulWidget {
@@ -14,8 +15,10 @@ class ChampionsByRolePage extends StatefulWidget {
 }
 
 class _ChampionsByRolePageState extends State<ChampionsByRolePage> {
-  List<Champion> champions = [];
+  List<Champion> allRoleChampions = [];
+  List<Champion> filteredChampions = [];
   bool isLoading = true;
+  String query = '';
 
   @override
   void initState() {
@@ -26,33 +29,54 @@ class _ChampionsByRolePageState extends State<ChampionsByRolePage> {
   Future<void> loadChampions() async {
     final allChampions = await ChampionService.fetchAll();
     setState(() {
-      champions =
+      allRoleChampions =
           allChampions.where((c) => c.tags.contains(widget.role)).toList();
+      filteredChampions = allRoleChampions;
       isLoading = false;
+    });
+  }
+
+  void applyFilter(String value) {
+    setState(() {
+      query = value;
+      filteredChampions = allRoleChampions
+          .where((c) => c.name.toLowerCase().contains(query.toLowerCase()))
+          .toList();
     });
   }
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.role, style: AppTheme.serif(size: 24)),
       ),
       body: isLoading
           ? const Center(child: CircularProgressIndicator())
-          : GridView.builder(
-              padding: const EdgeInsets.all(20),
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                crossAxisSpacing: 12,
-                mainAxisSpacing: 12,
-                childAspectRatio: 0.82,
+          : Padding(
+              padding: const EdgeInsets.fromLTRB(20, 8, 20, 0),
+              child: Column(
+                children: [
+                  ChampionsSearchBar(onChanged: applyFilter),
+                  const SizedBox(height: 14),
+                  Expanded(
+                    child: GridView.builder(
+                      padding: const EdgeInsets.only(bottom: 20),
+                      gridDelegate:
+                          const SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2,
+                        crossAxisSpacing: 12,
+                        mainAxisSpacing: 12,
+                        childAspectRatio: 0.82,
+                      ),
+                      itemCount: filteredChampions.length,
+                      itemBuilder: (context, index) {
+                        return ChampionCard(champion: filteredChampions[index]);
+                      },
+                    ),
+                  ),
+                ],
               ),
-              itemCount: champions.length,
-              itemBuilder: (context, index) {
-                return ChampionCard(champion: champions[index]);
-              },
             ),
     );
   }
